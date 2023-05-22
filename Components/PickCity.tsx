d9c2c62dc30e1f8fdb3c595a07cc0c115af5f0ed
Abolade Agboola <malikagboola18@gmail.com>
2023-05-22 04:46:44 +0100

    fast

--- Components/PickCity.tsx
+++ Components/PickCity.tsx
@@ -3,6 +3,8 @@ import { Country, State } from "country-state-city"
 import Select from "react-select";
 import {useState} from 'react'
 import { type } from "os";
+import { useRouter } from "next/navigation";
+import {GlobeIcon} from '@heroicons/react/solid'
 
 type option = {
   value:{
@@ -35,13 +37,30 @@ const options = Country.getAllCountries().map(country =>({
 
 function pickCity() {
 
-  const [selectedCountry, setSelectedCountry] = useState<option>(null)
-  const [selectedCity, setSelectedCity] = useState<cityOption>(null)
+  const [selectedCountry, setSelectedCountry] = useState<option>(null);
+  const [selectedCity, setSelectedCity] = useState<cityOption>(null);
+  const router = useRouter();
+  const handleSelectedCountry = (option: option) => {
+    setSelectedCountry(option);
+    setSelectedCity(null);
+  }
+
+
   return (
-    <div>
+    <div className="space-y-4">
+      <div className="space-y-2">
+      <div className="flex items-center space-x-2 text-white/80">
+        <GlobeIcon className="h-5 w-5 text-white"/>
+        <label htmlFor="country">Country</label>
+      </div>
+
       <Select 
+      className="text-black"
+      value={selectedCountry}
+      onChange={handleSelectedCountry}
       options={options}
       />
+      </div>
     </div>
   )
 }
