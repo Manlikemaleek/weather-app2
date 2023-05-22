be40174259be4b25128489d4741352840166db9a
Abolade Agboola <malikagboola18@gmail.com>
2023-05-22 04:26:58 +0100

    new

--- Components/PickCity.tsx
+++ Components/PickCity.tsx
@@ -1,9 +1,48 @@
+'use client';
 import { Country, State } from "country-state-city"
+import Select from "react-select";
+import {useState} from 'react'
+import { type } from "os";
 
+type option = {
+  value:{
+    longitude: string;
+  latitude: string;
+  isoCode: string;
+  };
+  label: string;
+} |null;
+
+type cityOption = {
+  value:{
+    longitude: string;
+  latitude: string;
+  name: string;
+  countryCode: string;
+  stateCode: string;
+  };
+  label: string;
+} |null;
+
+const options = Country.getAllCountries().map(country =>({
+  value:{
+    latitude: country.latitude,
+    longitude: country.longitude,
+    isoCode: country.isoCode,
+  },
+  label: country.name,
+}))
 
 function pickCity() {
+
+  const [selectedCountry, setSelectedCountry] = useState<option>(null)
+  const [selectedCity, setSelectedCity] = useState<cityOption>(null)
   return (
-    <div>pickCity</div>
+    <div>
+      <Select 
+      options={options}
+      />
+    </div>
   )
 }
 
