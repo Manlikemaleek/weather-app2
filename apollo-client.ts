8341766470566e47788127c114bf9326b7bfdac5
Abolade Agboola <malikagboola18@gmail.com>
2023-05-23 11:20:57 +0100

    new

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,7 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-let client : ApolloClient<any> | null= null;
-
 export const getClient = () => {
       const  client = new ApolloClient({
             uri: process.env.API_URL,
@@ -11,4 +9,6 @@ export const getClient = () => {
                 Authorization :`apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`,
             }
         })
+
+        return client;
     }
