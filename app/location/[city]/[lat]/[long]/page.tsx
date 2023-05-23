d4d5197740ead9a01a1828399adc83dddd8817be
Abolade Agboola <malikagboola18@gmail.com>
2023-05-23 11:38:45 +0100

    wow

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { getClient } from "@/apollo-client";
+import fetchWeather from "@/graphql/queries/fetchWeather";
+
+
 type Props = {
   params: {
     city: string;
@@ -7,7 +11,18 @@ type Props = {
   };
 };
 
-function weatherHome({ params: { city, lat, long } }: Props) {
+async function weatherHome({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+  const {data} = await client.query({
+    query: fetchWeather,
+    variables: {
+      current_weather: "true",
+      longitude: long,
+      latitude: lat,
+      timezone: "GMT"
+
+    }
+  })
   return (
     <div>
       welcome to WeatherMe {city} {lat} {long}
