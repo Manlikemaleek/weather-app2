33dab9edd3094fd178e0eaddb8bccdfc53e305f9
Abolade Agboola <malikagboola18@gmail.com>
2023-05-23 12:04:35 +0100

    new

---
+++ Components/callout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function callout() {
+  return (
+    <div>callout</div>
+  )
+}
+
+export default callout
--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { getClient } from "@/apollo-client";
 import fetchWeather from "@/graphql/queries/fetchWeather";
 
-
 type Props = {
   params: {
     city: string;
@@ -13,22 +12,35 @@ type Props = {
 
 async function weatherHome({ params: { city, lat, long } }: Props) {
   const client = getClient();
-  const {data} = await client.query({
+  const { data } = await client.query({
     query: fetchWeather,
     variables: {
       current_weather: "true",
       longitude: long,
       latitude: lat,
-      timezone: "GMT"
-
-    }
-  })
+      timezone: "GMT",
+    },
+  });
 
   const results: Root = data.myQuery;
 
   return (
     <div>
-      welcome to WeatherMe {city} {lat} {long}
+      {/* <info /> */}
+      <div>
+        <div className="pb-5">
+          <div className="pb-5">
+            <h2 className="text-xl font-bold">Overview Today</h2>
+            <p className="text-sm text-gray-500">
+              Updated last at : {""}
+              {new Date(results.current_weather.time).toLocaleString()} ({results.timezone})
+            </p>
+          </div>
+          <div>
+            {/* <callout /> */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
